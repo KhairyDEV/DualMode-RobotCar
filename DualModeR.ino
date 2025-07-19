f35f64fcaf8da7e648edb316653537851d8e316c
Salah Khairy <140529619+KhairyDEV@users.noreply.github.com>
2025-07-19 23:32:44 +0300

    DualModeR.ino

--- DualModeR.ino
+++ DualModeR.ino
@@ -1,14 +1,14 @@
-int in1 = 2; //Forward if HIGH
-int in2 = 3; //Backward if HIGH
-int in3 = 4; //Backward if HIGH
-int in4 = 7;//Forward if HIGH, These may change depending on the wiring diagram
-int ena = 6; //left motor speed analog OUT pin
-int enb = 5; // right motor speed analog OUT pin
+int in1 = 2; // Forward if HIGH
+int in2 = 3; // Backward if HIGH
+int in3 = 4; // Backward if HIGH
+int in4 = 7; // Forward if HIGH, these may change depending on the wiring diagram
+int ena = 6; // Left motor speed analog OUT pin
+int enb = 5; // Right motor speed analog OUT pin
 char Reading;
-#define echo 10 //reciver
-#define trig 11 //transmitter 
-int SPEED = 120; //adjustable speeed
-int mode = 1; //0 is ultrasonic (automatic), 1 is bluetooth (manually controlled)
+#define echo 10 // Receiver
+#define trig 11 // Transmitter
+int SPEED = 120; // Adjustable speed
+int mode = 1; // 0 is ultrasonic (automatic), 1 is Bluetooth (manually controlled)
 int distance;
 long duration;
 
@@ -22,22 +22,18 @@ pinMode(ena, OUTPUT);
 pinMode(enb, OUTPUT);
 pinMode(echo, INPUT);
 pinMode(trig, OUTPUT);
-
 }
-
 
 void loop() {
  if(Serial.available() > 0 && mode == 1 || Reading == 'A'){
   BLUETOOTH(); 
-
  }
  else if ( mode == 0 || Reading == 'D') {
   ULTRASONIC();
  }
-
 }
 
-void FORWARD() {-
+void FORWARD() {
   Serial.println("FORWARD");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
@@ -76,6 +72,7 @@ void BACKLEFT() {
   analogWrite(ena, SPEED -30);
   analogWrite(enb, SPEED);
 }
+
 void LEFT() {
   Serial.println("LEFT");
   digitalWrite(in1, LOW);
@@ -86,7 +83,8 @@ void LEFT() {
   analogWrite(enb, SPEED);
 }
 
-void FORLEFT(){ Serial.println("FORWARD LEFT");
+void FORLEFT(){
+  Serial.println("FORWARD LEFT");
   digitalWrite(in1, HIGH); 
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -105,7 +103,8 @@ void RIGHT() {
   analogWrite(enb, 0);
 }
 
- void FORIGHT(){ Serial.println("FRONT RIGHT");
+void FORIGHT(){
+  Serial.println("FRONT RIGHT");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -113,8 +112,6 @@ void RIGHT() {
   analogWrite(ena, SPEED);
   analogWrite(enb, SPEED - 30);
 }
-
- 
 
 void STOP() {
   Serial.println("STOP");
@@ -129,50 +126,46 @@ void STOP() {
 void BLUETOOTH(){
   Reading = Serial.read();
 
-
   switch(Reading){
-    case 'F' : FORWARD(); Serial.println("F");break;
-    case 'B' : BACKWARD(); Serial.println("B");break;
-    case 'R' : RIGHT(); Serial.println("R");break;
+    case 'F' : FORWARD(); Serial.println("F"); break;
+    case 'B' : BACKWARD(); Serial.println("B"); break;
+    case 'R' : RIGHT(); Serial.println("R"); break;
     case 'L' : LEFT(); Serial.println("L"); break;
     case 'S' : STOP(); Serial.println("S"); break;
     case 'G' : FORIGHT(); Serial.println("FR"); break;
     case 'H' : FORLEFT(); Serial.println("FL"); break;
     case 'J' : BACKRIGHT(); Serial.println("BR"); break;
     case 'K' : BACKLEFT(); Serial.println("BL"); break;
-    case 'A': mode = 1;  Serial.println("A");  break; //Bluetooth Activated
-    case 'D': mode = 0; Serial.println("D");  break; //Bluetooth Deactivated
- }
+    case 'A': mode = 1; Serial.println("A"); break; // Bluetooth Activated
+    case 'D': mode = 0; Serial.println("D"); break; // Bluetooth Deactivated
+  }
 }
 
-
 void ULTRASONIC(){
-digitalWrite(trig,LOW);
-delayMicroseconds(2);
-digitalWrite(trig,HIGH);
-delayMicroseconds(10);
-digitalWrite(trig,LOW);
-duration = pulseIn(echo,HIGH);
-distance = (duration * 0.0343)/2;
-Serial.print(distance);
-Serial.println(" cm");
-Reading = Serial.read();
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = (duration * 0.0343)/2;
+  Serial.print(distance);
+  Serial.println(" cm");
+  Reading = Serial.read();
 
-
-if ( distance < 10){
-  STOP(); Serial.println("S");
-  delay(200);
-  BACKWARD(); Serial.println("B"); 
-  delay(500);
-  LEFT(); Serial.println("L");
-  delay(800); //adjustable delays
+  if (distance < 10){
+    STOP(); Serial.println("S");
+    delay(200);
+    BACKWARD(); Serial.println("B"); 
+    delay(500);
+    LEFT(); Serial.println("L");
+    delay(800); // Adjustable delays
   }
-  else if(Reading == 'D'){
-    BLUETOOTH(); //if 'D' is called then switch to bluetooth
-
+  else if (Reading == 'D'){
+    BLUETOOTH(); // If 'D' is called then switch to Bluetooth
   }
- else {
-  FORWARD(); Serial.println("F");
- }
-delay(100);
+  else {
+    FORWARD(); Serial.println("F");
+  }
+  delay(100);
 }
